706904334c5b5052738150e7dda322b60a45a08b
adospinoiu <alex.dospinoiu@gmail.com>
2021-02-07 06:57:42 -0700

    Created routes folder and api and html Routes.js files. Added the route to the index.html file in htmlRoutes.js

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,7 @@
+const path = require("path");
+const router = require("express").Router();
+
+// This is the default page that will load when the user first arrives at the application
+router.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "..public/index.html"));
+})
