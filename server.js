5699b3ebc8839747750e9935c41c718cc6cdd59c
adospinoiu <alex.dospinoiu@gmail.com>
2021-02-07 05:58:35 -0700

    Created server.js file. Started up the express back-end (server).

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const morgan = require("morgan");
+
+// Initialize the application to create a port
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(morgan('tiny'));
+
+// Start up the server
+app.listen(PORT, () => console.log('Listening at http://localhost:${PORT}'));
