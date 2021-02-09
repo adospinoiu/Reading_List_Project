4202de014f1f486090c4f427be436b31fa8d12f6
adospinoiu <alex.dospinoiu@gmail.com>
2021-02-09 05:37:11 -0700

    Added the /list route to the htmlRoutes.js file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,5 +3,11 @@ const router = require("express").Router();
 
 // This is the default page that will load when the user first arrives at the application
 router.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "..public/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 })
+
+router.get("/list", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/list.html"));
+})
+
+module.exports = router;
