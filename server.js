1ef590dc1047ef7de252a2c2e5d0c25a2da87f12
adospinoiu <alex.dospinoiu@gmail.com>
2021-02-10 05:34:42 -0700

    Added code to server.js in order to get the /list route to work on a request

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const htmlRoutes = require("./routes/htmlRoutes.js");
+
 // Initialize the application to create a port
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +11,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(morgan('tiny'));
+
+//Routes to the html and api .js files
+app.use("/", htmlRoutes);
 
 // Start up the server
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
